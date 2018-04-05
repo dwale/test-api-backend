04e9217790f61a3e30ae97eb46272429fcb10813
Laleye Olamide <laleyeolamide@yahoo.com>
2018-04-05 12:24:58 +0100

    Update Result.java

--- src/main/java/com/calculator/api/bean/Result.java
+++ src/main/java/com/calculator/api/bean/Result.java
@@ -3,6 +3,8 @@ package com.calculator.api.bean;
 /**
  * Created by sq_09 on 1/17/2018.
  */
+
+//This simply creates a class of the object to be returned by the api
 public class Result {
 
     private Double Answer;
